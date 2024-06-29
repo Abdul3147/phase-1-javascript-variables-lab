fda5b2cc2001e50a519f2f903b479362476be655
Abdul3147 <abdallahussein228@gmail.com>
2024-06-29 15:40:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+//let companyName = "Scuber";
+const companyName = "Scuber";
+
+//let mostProfitableNeighborhood = "chelsea";
+const mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";   
